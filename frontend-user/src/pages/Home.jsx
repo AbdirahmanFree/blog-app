d2649b83b988ba9f188b1e9162ff3b9265f0991d
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-06-04 20:04:21 -0400

    Built home page to display all blog posts - modified verify token to to check if tokens are expired - but get commentliike and getpostlike routes and middleware to check iif user liked a post or comment - modifed getposts to return username of post creator in response - created fetch post function that runs on each render of home to fetch posts and display them - created layout and styling of posts - created sticky naviigation menu header -updated import in login

--- frontend-user/src/pages/Home.jsx
+++ frontend-user/src/pages/Home.jsx
@@ -1,14 +1,52 @@
 import NavBar from "@/components/NavBar";
 import { UseUserAuth } from "@/hooks/useUserAuth";
+import axiosInstance from "@/utils/axiosInstance";
+import { useEffect, useState } from "react";
+import { FaRegThumbsUp } from "react-icons/fa";
+import { FaThumbsUp } from "react-icons/fa";
 
 function Home(){
     UseUserAuth()
+    const [posts,setPosts] = useState([])
+    useEffect(()=>{
+        const fetchPosts = async () => {
+            const postRes = await axiosInstance.get("/api/posts")
+            const postsArray = postRes.data.posts
+            console.log(postsArray)
+            postsArray.forEach(async (post)=>{
+                const likedPostRes = await axiosInstance.get(`/api/posts/${post.id}/like`)
+                const liked = likedPostRes.data
+                const date = new Date(post.updated_at)
+                const formatedDate = date.toLocaleDateString("en-US",{month: "long", day: "numeric", year: "numeric"})
+                post.date = formatedDate
+                post.liked = liked
+                setPosts([...posts,post])
+            })
+        }
+        fetchPosts()
+    },[])
+
+    
 
     return(
-    <>
+    <div className="flex flex-col m-0 gap-8 w-full">
         <NavBar/>
-        <h1>Home</h1>
-    </>
+        {posts.length > 0 ? (
+            posts.map((post) => {
+                return(<div key={post.id} className=" bg-gray-50 m-2 border-2 h-50 rounded-2xl flex flex-col p-4 gap-2 ">
+                    <div>
+                        <h1 className="m-0 text-2xl">{post.title}</h1>
+                        <span className="text-gray-600">{post.user.username} • {post.date}</span>
+                    </div>
+                    <p className="p-4 text-gray-800 italic flex justify-center ">
+                        {post.content.length > 50 ? `${post.content.slice(0,50)} \n Read More ... ` : post.content}
+                    </p>
+                </div>)
+            })
+        ): (
+            <div>No posts</div>
+        )}
+    </div>
     )
 }
 
